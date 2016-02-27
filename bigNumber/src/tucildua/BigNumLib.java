a6ee11f86fa6777d35cdf51e2bade6ea22b2e54c
Rian Fakhrusy <staroushi38@gmail.com>
2016-02-28 01:03:28 +0700

    Adding Karatsuba multiplication

--- bigNumber/src/tucildua/BigNumLib.java
+++ bigNumber/src/tucildua/BigNumLib.java
@@ -1,6 +1,26 @@
 package tucildua;
 
-public class BigNumLib {     
+public class BigNumLib {
+    //konstruktor
+    public BigNumLib(){
+        this.count = 0;
+        this.product = 0;
+    }
+    //setter
+    public void setCount(int c){
+        this.count = c;
+    }
+    public void setProduct(int c){
+        this.product = c;
+    }
+    //getter
+    public int getCount(){
+        return this.count;
+    }
+    public int getProduct(){
+        return this.product;
+    }
+    
     // mengembalikan true jika string merupakan bilangan negatif dan false bila positif atau nol
     public static boolean isMinus(String s){
         return (s.charAt(0)=='-');
@@ -8,7 +28,7 @@ public class BigNumLib {
     
     //mengembalikan true string yang pertama lebih kecil dari string yang kedua dan false jika sebaliknya atau sama
     public static boolean smallerthan(String s1, String s2){
-        for(int i = 0; i < s1.length(); i++){
+        for(int i = 0; i < s1.length()-1; i++){
             if(s2.charAt(i) > s1.charAt(i)){
                 return true;
             } else if (s2.charAt(i) < s1.charAt(i)){
@@ -19,17 +39,21 @@ public class BigNumLib {
     }
     
     //menjumlahkan dua buah bilangan
-    public static String plus(String s1, String s2){
+    public String plus(String s1, String s2){
         String hsl = "";
         if (!isMinus(s1)&&!isMinus(s2)){ //kedua bilangan positif
             String s[] = addZero(s1,s2);
             boolean carry = false;
             for (int i=s[0].length()-1;i>=0;i--){
                 int temp = (s[0].charAt(i) - '0') + (s[1].charAt(i) - '0');
-                if (carry) temp += 1;
+                this.count++;
+                if (carry) {
+                    temp += 1;
+                    this.count++;
+                }
                 if (temp > 9){
                     carry = true;
-                    temp -= 10;
+                    temp %= 10;
                 } else 
                     carry = false;    
                 hsl = temp + hsl;
@@ -47,21 +71,25 @@ public class BigNumLib {
     }
     
     //mengurangkan bilangan pertama dengan bilangan kedua
-    public static String minus(String s1, String s2){
+    public String minus(String s1, String s2){
         String hsl = "";
         if (!isMinus(s1)&&!isMinus(s2)){ //kedua bilangan positif
             String min = "";
-            if (smallerthan(s1,s2)){
-                String temp = s1;
-                s1 = s2;
-                s2 = temp;
+            String s[] = addZero(s1,s2);
+            if (smallerthan(s[0],s[1])){
+                String temp = s[0];
+                s[0] = s[1];
+                s[1] = temp;
                 min = "-";
             }
             boolean mincarry = false;
-            String s[] = addZero(s1,s2);
             for (int i=s[0].length()-1;i>=0;i--){
                 int temp = (s[0].charAt(i) - '0') - (s[1].charAt(i) - '0');
-                if (mincarry) temp -= 1;
+                this.count++;
+                if (mincarry) {
+                    temp -= 1;
+                    this.count++;
+                }
                 if (temp < 0){
                     mincarry = true;
                     temp += 10;
@@ -72,7 +100,7 @@ public class BigNumLib {
             hsl = min + hsl.replaceFirst("^0+(?!$)", "");
         } else if (!isMinus(s1)&&isMinus(s2)){ //bilangan pertama positif, bilangan kedua negatif
             hsl = plus(s1,s2.substring(1));
-        } else if (isMinus(s1)&&!isMinus(s2)){  //bilangan pertama negatif, bilangan kedua positif
+        } else if (isMinus(s1)&&!isMinus(s2)){ //bilangan pertama negatif, bilangan kedua positif
             hsl = '-' + plus(s1.substring(1),s2);
         } else { //kedua bilangan negatif
             hsl = minus(s2.substring(1),s1.substring(1));
@@ -80,8 +108,56 @@ public class BigNumLib {
         return hsl;
     }
     
+    //mengalikan bilangan pertama dengan bilangan kedua
+    public String multiply(String s1, String s2){
+        if (!isMinus(s1)&&!isMinus(s2)){ //kedua bilangan positif
+            String s[] = addZero(s1,s2);
+            return karatsuba(s[0],s[1]);
+        } else if (!isMinus(s1)&&isMinus(s2)){ //bilangan pertama positif, bilangan kedua negatif
+            String s[] = addZero(s1,s2.substring(1));
+            return '-' + karatsuba(s[0],s[1]);
+        } else if (isMinus(s1)&&!isMinus(s2)){ //bilangan pertama negatif, bilangan kedua positif
+            String s[] = addZero(s1.substring(1),s2);
+            return '-' + karatsuba(s[0],s[1]);
+        }
+        //kedua bilangan negatif
+        String s[] = addZero(s1.substring(1),s2.substring(1));
+        return karatsuba(s[0],s[1]);
+    }
+    
+    //menggunakan algoritma karatsuba
+    public String karatsuba(String s1, String s2){
+        if ((s1.length()==1)||(s2.length()==1)){
+            this.product++;
+            return (Integer.parseInt(s1)*Integer.parseInt(s2))+"";
+        }
+        // calculate the number size
+        int m = Math.max(s1.length(), s1.length());
+        int m2 = m/2;
+        // split the digit in the middle
+        String a = s1.substring(0, m2);
+        String b = s1.substring(m2, s1.length());
+        String c = s2.substring(0, m2);
+        String d = s2.substring(m2, s2.length());
+        //recursive function
+        String p = multiply(a, c);
+        String q = multiply(b, d);
+        String r = multiply(plus(a,b),plus(c,d));
+        //compute power function
+        String powp = p;
+        for (int i=0;i<b.length()+d.length();i++){
+            powp += '0';
+        }
+        String rpq = minus(minus(r,p),q);
+        for (int i=0;i<Math.max(b.length(),d.length());i++){
+            rpq += '0';
+        }
+        
+        return plus(plus(powp,rpq),q);
+    }
+    
     //menambahkan angka nol pada angka yang memiliki jumlah digit lebih sedikit hingga jumlah digitnya sama
-    public static String[] addZero(String s1, String s2){
+    public String[] addZero(String s1, String s2){
         if (s1.length()>s2.length()){
             int limit = s1.length()-s2.length();
             for (int i=0;i<limit;i++){
@@ -97,8 +173,10 @@ public class BigNumLib {
     }
     /*
     public static void main(String args[]){
-        String s1 = "7654729850328997631007285998163550104";
-        String s2 = "5980139243970186632651869926335829102";
-        System.out.println(plus(s1, s2));
+        String s1 = "31313131";//"7654729850328997631007285998163550104";
+        String s2 = "43434343";//"5980139243970186632651869926335829102";
+        System.out.println(multiply(s1, s2));
     }*/
+    private int count;
+    private int product;
 }
--- bigNumber/src/tucildua/bigJFrame.java
+++ bigNumber/src/tucildua/bigJFrame.java
@@ -48,8 +48,12 @@ public class bigJFrame extends javax.swing.JFrame {
         sizeInput = new javax.swing.JTextField();
         generaterNumber = new javax.swing.JToggleButton();
         jDesktopPane1 = new javax.swing.JDesktopPane();
-        pilihNumber = new javax.swing.JComboBox<>();
-        signNumber = new javax.swing.JComboBox<>();
+        pilihNumber = new javax.swing.JComboBox<String>();
+        signNumber = new javax.swing.JComboBox<String>();
+        timeField = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        operationField = new javax.swing.JTextField();
+        jLabel7 = new javax.swing.JLabel();
 
         javax.swing.GroupLayout jDialog1Layout = new javax.swing.GroupLayout(jDialog1.getContentPane());
         jDialog1.getContentPane().setLayout(jDialog1Layout);
@@ -92,6 +96,11 @@ public class bigJFrame extends javax.swing.JFrame {
         kali.setFont(new java.awt.Font("Cantarell", 1, 18)); // NOI18N
         kali.setForeground(new java.awt.Color(254, 254, 254));
         kali.setText("Kali");
+        kali.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                kaliActionPerformed(evt);
+            }
+        });
 
         tambah.setBackground(new java.awt.Color(248, 152, 23));
         tambah.setFont(new java.awt.Font("Cantarell", 1, 18)); // NOI18N
@@ -109,17 +118,20 @@ public class bigJFrame extends javax.swing.JFrame {
 
         hasil.setColumns(20);
         hasil.setFont(new java.awt.Font("Cantarell", 1, 18)); // NOI18N
+        hasil.setLineWrap(true);
         hasil.setRows(5);
         hasil.setWrapStyleWord(true);
         jScrollPane1.setViewportView(hasil);
 
         number2.setColumns(20);
         number2.setFont(new java.awt.Font("Cantarell", 1, 18)); // NOI18N
+        number2.setLineWrap(true);
         number2.setRows(5);
         jScrollPane2.setViewportView(number2);
 
         number1.setColumns(20);
         number1.setFont(new java.awt.Font("Cantarell", 1, 18)); // NOI18N
+        number1.setLineWrap(true);
         number1.setRows(5);
         number1.setWrapStyleWord(true);
         jScrollPane3.setViewportView(number1);
@@ -150,7 +162,7 @@ public class bigJFrame extends javax.swing.JFrame {
             }
         });
 
-        pilihNumber.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Number1", "Number2" }));
+        pilihNumber.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Number1", "Number2" }));
         pilihNumber.setToolTipText("");
         pilihNumber.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -158,7 +170,23 @@ public class bigJFrame extends javax.swing.JFrame {
             }
         });
 
-        signNumber.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Positif", "Negatif" }));
+        signNumber.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Positif", "Negatif" }));
+
+        timeField.setEditable(false);
+        timeField.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        timeField.setEnabled(false);
+
+        jLabel6.setFont(new java.awt.Font("Cantarell", 1, 24)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(0, 127, 169));
+        jLabel6.setText("Time :");
+
+        operationField.setEditable(false);
+        operationField.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        operationField.setEnabled(false);
+
+        jLabel7.setFont(new java.awt.Font("Cantarell", 1, 24)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(0, 127, 169));
+        jLabel7.setText("Operation :");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -167,38 +195,50 @@ public class bigJFrame extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(26, 26, 26)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(18, 18, 18)
-                        .addComponent(jLabel3)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
-                        .addComponent(tambah, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(kurang, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(18, 18, 18)
-                        .addComponent(kali, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(6, 6, 6))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel4)
-                            .addComponent(jLabel1)
-                            .addComponent(jLabel2))
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel5)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(sizeInput, javax.swing.GroupLayout.PREFERRED_SIZE, 201, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addGap(18, 18, 18)
+                                .addComponent(jLabel3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
+                                .addComponent(tambah, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(kurang, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(kali, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(6, 6, 6))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(jLabel5)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(sizeInput, javax.swing.GroupLayout.PREFERRED_SIZE, 201, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(signNumber, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jDesktopPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(pilihNumber, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(generaterNumber, javax.swing.GroupLayout.PREFERRED_SIZE, 155, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jScrollPane2)
+                            .addComponent(jScrollPane1)
+                            .addComponent(jScrollPane3)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel1)
+                                    .addComponent(jLabel2))
+                                .addGap(0, 0, Short.MAX_VALUE)))
+                        .addGap(37, 37, 37))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel4)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLabel7)
+                        .addGap(18, 18, 18)
+                        .addComponent(operationField, javax.swing.GroupLayout.PREFERRED_SIZE, 176, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel6)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(signNumber, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jDesktopPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(pilihNumber, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(generaterNumber, javax.swing.GroupLayout.PREFERRED_SIZE, 155, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(jScrollPane2)
-                    .addComponent(jScrollPane1)
-                    .addComponent(jScrollPane3))
-                .addGap(37, 37, 37))
+                        .addComponent(timeField, javax.swing.GroupLayout.PREFERRED_SIZE, 176, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(50, 50, 50))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -238,8 +278,16 @@ public class bigJFrame extends javax.swing.JFrame {
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(15, 15, 15)
-                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel4)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(timeField, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel6)
+                            .addComponent(operationField, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel7))
+                        .addGap(1, 1, 1)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(24, 24, 24))
@@ -247,38 +295,15 @@ public class bigJFrame extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-    public int charToInt(char c) {
-	switch (c) {
-		case '0' : return 0; 
-		case '1' : return 1;
-		case '2' : return 2;
-		case '3' : return 3; 
-		case '4' : return 4;
-		case '5' : return 5;
-		case '6' : return 6;
-		case '7' : return 7;
-		case '8' : return 8;
-		case '9' : return 9;
-		default : return -1;
-	}
-    }
-    public char intToChar(int x) {
-	switch (x) {
-		case 0 : return '0'; 
-		case 1 : return '1';
-                case 2 : return '2';
-		case 3 : return '3'; 
-		case 4 : return '4';
-		case 5 : return '5';
-		case 6 : return '6';
-		case 7 : return '7';
-		case 8 : return '8';
-		case 9 : return '9';
-		default : return ' ';
-	}
-    }
+
     private void kurangActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_kurangActionPerformed
-        hasil.setText(BigNumLib.minus(number1.getText(), number2.getText()));
+        long start = System.nanoTime();
+        b = new BigNumLib();
+        hasil.setText(b.minus(number1.getText(), number2.getText()));
+        operationField.setText(b.getCount()+" times");
+        long end = System.nanoTime();
+        long micro = (end - start) / 1000;
+        timeField.setText(micro + " microseconds");
     }//GEN-LAST:event_kurangActionPerformed
 
     private void formMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_formMouseDragged
@@ -286,40 +311,28 @@ public class bigJFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_formMouseDragged
     
     private void tambahActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tambahActionPerformed
-        hasil.setText(BigNumLib.plus(number1.getText(), number2.getText()));
+        long start = System.nanoTime();
+        b = new BigNumLib();
+        hasil.setText(b.plus(number1.getText(), number2.getText()));
+        operationField.setText(b.getCount()+" times");
+        long end = System.nanoTime();
+        long micro = (end - start) / 1000;
+        timeField.setText(micro + " microseconds");
     }//GEN-LAST:event_tambahActionPerformed
 
     private void generaterNumberActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_generaterNumberActionPerformed
         int sizeint = Integer.parseInt(sizeInput.getText());
-        int START = 1;
-        int END = 9;
-        String Sc;
+        String s = "";
         Random random = new Random();
-        char hasilRandom[] = new char[sizeint];
-        if (signNumber.getSelectedIndex()==0) {
-            //hasilRandom[0]='+';
-        } else {
-            hasilRandom[0]='-';
-        }
         for (int idx = 0; idx < sizeint; idx++){
-            if (START > END) {
-                throw new IllegalArgumentException("Start cannot exceed End.");
-            }
-            //get the range, casting to long to avoid overflow problems
-            long range = (long)END - (long)START + 1;
-            // compute a fraction of the range, 0 <= frac < range
-            long fraction = (long)(range * random.nextDouble());
-            int randomNumber =  (int)(fraction + START);  
-            char c = Character.forDigit(randomNumber, 10);
-            hasilRandom[idx] = c;
+            int randomNumber = (idx!=0)?random.nextInt(10):random.nextInt(9)+1; 
+            s += randomNumber;
         }
-       // Sc = String.valueOf(hasilRandom);
        if (pilihNumber.getSelectedIndex()==0) {
-           number1.setText(String.valueOf(hasilRandom));
+           number1.setText(((signNumber.getSelectedIndex()==1)?"-":"") + s);
        } else {
-           number2.setText(String.valueOf(hasilRandom));
+           number2.setText(((signNumber.getSelectedIndex()==1)?"-":"") + s);
        }
-        
     }//GEN-LAST:event_generaterNumberActionPerformed
 
     private void sizeInputActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sizeInputActionPerformed
@@ -329,6 +342,16 @@ public class bigJFrame extends javax.swing.JFrame {
     private void pilihNumberActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pilihNumberActionPerformed
 
     }//GEN-LAST:event_pilihNumberActionPerformed
+
+    private void kaliActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_kaliActionPerformed
+        long start = System.nanoTime();
+        b = new BigNumLib();
+        hasil.setText(b.multiply(number1.getText(), number2.getText()));
+        operationField.setText(b.getProduct()+" times");
+        long end = System.nanoTime();
+        long micro = (end - start) / 1000;
+        timeField.setText(micro + " microseconds");
+    }//GEN-LAST:event_kaliActionPerformed
 
     /**
      * @param args the command line arguments
@@ -364,7 +387,8 @@ public class bigJFrame extends javax.swing.JFrame {
             }
         });
     }
-
+    
+    private BigNumLib b;
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JToggleButton generaterNumber;
     private javax.swing.JTextArea hasil;
@@ -376,6 +400,8 @@ public class bigJFrame extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
@@ -383,9 +409,11 @@ public class bigJFrame extends javax.swing.JFrame {
     private javax.swing.JButton kurang;
     private javax.swing.JTextArea number1;
     private javax.swing.JTextArea number2;
+    private javax.swing.JTextField operationField;
     private javax.swing.JComboBox<String> pilihNumber;
     private javax.swing.JComboBox<String> signNumber;
     private javax.swing.JTextField sizeInput;
     private javax.swing.JButton tambah;
+    private javax.swing.JTextField timeField;
     // End of variables declaration//GEN-END:variables
 }
